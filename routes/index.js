a972edfe0264c7d0dfd4d2671ac5c7e5a8f0ea48
hariansyah <hariansyahhari2@gmail.com>
2022-02-10 23:07:30 +0700

    Change documentation site

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const userServices = require('../services/user')
 
 router.get('/', function (req, res, next) {
-    res.status(301).redirect("https://documenter.getpostman.com/view/10433449/TVzNKKqH");
+    res.status(301).redirect("https://documenter.getpostman.com/view/10433449/UVeMGNJo");
 })
 router.post('/authenticate', function (req, res, next) {
     userServices.authenticate(req, res)
